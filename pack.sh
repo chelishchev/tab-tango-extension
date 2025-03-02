46bdb681db705f0fd27da8fa9814828be849f7bd
Ivan Chelishchev <chelishchev@gmail.com>
2025-03-02 15:08:55 +0200

    Update .gitignore, manifest description, and packaging script for Tab Tango

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 V=$(cat ./manifest.json | jq -Mr .version)
-rm -f "./builds/duplicate-tab-$V.zip"
-zip -r -FS "./builds/duplicate-tab-$V.zip" ./assets/ ./background_script.js ./manifest.json ./options.html ./options_script.js -x '*.DS_Store' -x '*Thumbs.db'
+rm -f "./builds/tab-tango-$V.zip"
+zip -r -FS "./builds/tab-tango-$V.zip" ./assets/ ./background_script.js ./manifest.json ./options.html ./options_script.js -x '*.DS_Store' -x '*Thumbs.db'
